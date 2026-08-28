d62fdd9c4d8eefa86c4a2a9ebc6927dd5a898204
Natasha Ulery <natashaulery@gmail.com>
2026-08-28 20:38:39 +0000

    Update top-left logo to new enso image

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import { Image } from "@/components/ui/image";
 
 // Brand mark: the user's enso logo image, fixed in the top-left corner.
 const LOGO_URL =
-  "https://media.base44.com/images/public/6a91be5ced6058323eb21f7d/c5405678d_IMG_7694.jpeg";
+  "https://media.base44.com/images/public/6a91be5ced6058323eb21f7d/767c033d8_3E798DE5-95A7-49F9-879D-C8303E1BB25C.png";
 
 export default function Logo({ size = 36, className = "" }) {
   return (
