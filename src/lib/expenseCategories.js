af4f48dbff13de9274e92403980c56c076fcb6f4
Natasha Ulery <natashaulery@gmail.com>
2026-08-29 12:47:42 +0000

    External agent changes

---
+++ src/lib/expenseCategories.js
@@ -0,0 +1,26 @@
+export const EXPENSE_CATEGORIES = [
+  "Art Materials & Supplies",
+  "Paper & Print Media",
+  "Ink & Printing Supplies",
+  "Frames & Display",
+  "Packaging & Shipping Supplies",
+  "Equipment & Tools",
+  "Photography Equipment",
+  "Software & Subscriptions",
+  "Phone / Internet",
+  "Advertising & Marketing",
+  "Shipping & Postage",
+  "Office & Business",
+  "Other Business Expense",
+  // Legacy categories kept for existing records.
+  "Inventory / Frames",
+  "Printing Supplies",
+  "Packaging",
+  "Equipment",
+  "Office Expense",
+  "Advertising",
+  "Shipping",
+  "Other",
+];
+
+export const EXPENSE_CATEGORY_FILTERS = ["All", ...EXPENSE_CATEGORIES];
