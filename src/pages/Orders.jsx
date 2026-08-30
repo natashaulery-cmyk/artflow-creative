f9bc8be51966a84dc7d35d3c6c269f9d6525ccc6
Natasha Ulery <natashaulery@gmail.com>
2026-08-30 22:27:40 +0000

    External agent changes

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -37,7 +37,7 @@ export default function Orders() {
   const importEmailSales = async () => {
     setImportingEmail(true);
     try {
-      const directSources = ["syncVintedPro", "syncDepopPartner", "syncEtsy"];
+      const directSources = ["syncVintedPro", "syncDepopPartner", "syncEtsy", "syncEbay"];
       const directMessages = [];
 
 
